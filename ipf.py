270e9ce89230daa78eb6cabf789a6a418c16f442
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2023-10-27 16:25:39 +0200

    inverse pole figure try

--- ipf.py
+++ ipf.py
@@ -1,310 +1,178 @@
-from xfab import tools, symmetry
 import numpy as np
-import os
-from ImageD11.grain import read_grain_file
 import matplotlib.pyplot as plt
+import xfab.symmetry
+from shapely import Polygon, Point
+from scipy.interpolate import griddata
 from scipy.spatial.transform import Rotation
 
-# from matplotlib import rc
-# rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-# rc('text', usetex=True)
+class inverse_pole_figure(object):
+
+    def __init__(self, crystal_system, view_axis):
+        crystal_dict = {        'triclinic': 1,
+                                'monoclinic': 2,
+                                'orthorhombic': 3,
+                                'tetragonal': 4,
+                                'trigonal': 5,
+                                'hexagonal': 6,
+                                'cubic': 7    }
+
+        fundamental_dict = {
+            'triclinic': [(-1,0,0), (1, 1, 0), (1, -1, 0)],
+            'monoclinic': [(-1,0,0), (0, 1, 0), (1, 0, 0)],
+            'orthorhombic': [(0, 0, 1), (0, 1, 0), (1, 0, 0)],
+            'tetragonal': [(0, 0, 1), (0, 1, 0), (1, 1, 0)],
+            'trigonal': [(0,0,1), (0,1,0), (1,0,0)],
+            'hexagonal': [(0,0,1), (1,0,0), (2,-1,0)],
+            'cubic':[(0,0,1), (0,1,1), (1,1,1)]
+            }
+
+        self.crystal_system = crystal_system
+        self.P = xfab.symmetry.permutations(crystal_dict[crystal_system])
+        self.view_axis = view_axis / np.linalg.norm(view_axis)
+        self.fundamental_triangle = np.array(fundamental_dict[crystal_system]).T
+
+        self._p = Polygon( self.get_fundamental_triangle(resolution=150).T )
+
+    def colorbar(self):
+        ori = [ Rotation.random().as_matrix().T for _ in range(2000) ]
+        points = self.get_reciprocal_points( ori, filter_by_fundamental_zone=False )
+        xy = self.stereographic_projection( points )
+        edges = self.get_fundamental_triangle()
+        rgb = self.get_colors(points)
+        xmin,xmax = np.min(edges[0,:]), np.max(edges[0,:])
+        ymin,ymax = np.min(edges[1,:]), np.max(edges[1,:])
+        grid_x, grid_y = np.meshgrid(np.linspace(xmin, xmax, 128), np.linspace(ymin, ymax, 128), indexing='ij')
+
+        mask = np.zeros( grid_x.shape,  dtype=bool)
+        for i in range(grid_x.shape[0]):
+            for j in range(grid_y.shape[1]):
+                mask[i,j] = Point( [grid_x[i,j], grid_y[i,j]] ).within( self._p )
+
+        grid_z1 = griddata(xy.T, rgb.T, (grid_x, grid_y), method='linear')
+        grid_z1[~mask] = np.nan
+
+        fig,ax = plt.subplots(1,1, figsize=(9,9))
+        ax.axis('equal')
+        ax.plot(edges[0], edges[1], 'gray', linewidth=int(10*64./grid_y.shape[0]))
+        ax.axis('off')
+        plt.pcolormesh(grid_x, grid_y, grid_z1)
+        ft = self.fundamental_triangle
+        xyf = self.stereographic_projection(self.fundamental_triangle)
+        ax.annotate( str(ft[0,0])+' '+str(ft[1,0])+' '+str(ft[2,0]), (xyf[0,0]+0.01,xyf[1,0]-0.01), fontsize=20, annotation_clip=False )
+        ax.annotate( str(ft[0,1])+' '+str(ft[1,1])+' '+str(ft[2,1]), (xyf[0,1]-0.01,xyf[1,1]+0.01), fontsize=20, annotation_clip=False)
+        ax.annotate( str(ft[0,2])+' '+str(ft[1,2])+' '+str(ft[2,2]), (xyf[0,2]+0.01,xyf[1,2]+0.01), fontsize=20 , annotation_clip=False)
+        plt.show()
 
 
-class RodriguezRotator(object):
-    """Object for rotating vectors in the plane described by yhe unit normal rotation_axis.
+    def show( self, ubi_matrices, point_size=50, alpha=1 ):
+        points = self.get_reciprocal_points( ubi_matrices )
+        xy = self.stereographic_projection( points )
+        edges = self.get_fundamental_triangle()
+        rgb = self.get_colors(points)
 
-    Args:
-        rotation_axis (:obj:`numpy array`): A unit vector in 3d euclidean space (``shape=(3,)``)
+        fig,ax = plt.subplots(1,1, figsize=(8,8))
+        ax.axis('equal')
+        ax.scatter(xy[0], xy[1], c=rgb.T, s=point_size, alpha=alpha, linewidth=0)
+        ax.plot(edges[0], edges[1], 'k-')
+        ax.axis('off')
+        ft = self.fundamental_triangle
+        xyf = self.stereographic_projection(self.fundamental_triangle)
+        ax.annotate( str(ft[0,0])+' '+str(ft[1,0])+' '+str(ft[2,0]), (xyf[0,0]+0.01,xyf[1,0]-0.01), fontsize=20 , annotation_clip=False)
+        ax.annotate( str(ft[0,1])+' '+str(ft[1,1])+' '+str(ft[2,1]), (xyf[0,1]-0.01,xyf[1,1]+0.01), fontsize=20, annotation_clip=False)
+        ax.annotate( str(ft[0,2])+' '+str(ft[1,2])+' '+str(ft[2,2]), (xyf[0,2]+0.01,xyf[1,2]+0.01), fontsize=20 , annotation_clip=False)
+        va = self.view_axis
+        plt.show()
 
-    Attributes:
-        rotation_axis (:obj:`numpy array`): A unit vector in 3d euclidean space (``shape=(3,)``)
-        K (:obj:`numpy array`): (``shape=(3,3)``)
-        K2 (:obj:`numpy array`): (``shape=(3,3)``)
-        I (:obj:`numpy array`): (``shape=(3,3)``)
+    def get_fundamental_triangle(self, resolution = 100 ):
+        t = np.linspace(0, 1, resolution)
+        U = self.fundamental_triangle[:,0:1]
+        V = self.fundamental_triangle[:,1:2]
+        W = self.fundamental_triangle[:,2:3]
+        U = U / np.linalg.norm(U)
+        V = V / np.linalg.norm(V)
+        W = W / np.linalg.norm(W)
+        l1 = U*(1 - t) + V*t
+        l2 = V*(1 - t) + W*t
+        l3 = W*(1 - t) + U*t
+        points = np.concatenate( (l1, l2, l3), axis=1 )
+        q = self.stereographic_projection(l3)
+        return self.stereographic_projection(points)
 
-    """
-
-    def __init__(self, rotation_axis):
-        assert np.allclose(np.linalg.norm(rotation_axis),
-                           1), "The rotation axis must be length unity."
-        self.rotation_axis = rotation_axis
-        rx, ry, rz = self.rotation_axis
-        self.K = np.array([[0, -rz, ry],
-                           [rz, 0, -rx],
-                           [-ry, rx, 0]])
-        self.K2 = self.K.dot(self.K)
-
-    def get_rotation_matrix(self, rotation_angle):
-        return np.eye(3, 3) + np.sin(rotation_angle) * self.K + \
-            (1 - np.cos(rotation_angle)) * self.K2
-
-    def __call__(self, vectors, rotation_angle):
-        """Rotate a vector in the plane described by v1 and v2 towards v2 a fraction s=[0,1].
-
-        Args:
-            vectors (:obj:`numpy array`): A set of vectors in 3d euclidean space to be rotated (``shape=(3,N)``)
-            rotation_angle (:obj:`float`): Radians to rotate vectors around the rotation_axis (positive rotation).
-
-        Returns:
-            Rotated vectors (:obj:`numpy array`) of ``shape=(3,N)``.
-
+    def stereographic_projection(self, points):
+        """pole at z=-1, plane at z=0
         """
-        R = self.get_rotation_matrix(rotation_angle)
-        return R.dot(vectors)
 
-def _backtrace(coord, south_pole):
-    n = coord - south_pole
-    n = n / np.linalg.norm(n)
-    #print(n)
-    # k = coord + s * n
-    # k*k == 1
-    # coord*coord + 2 * s * n * coord + s*s - 1 == 0
-    q =  coord.dot(coord) - 1
-    p =  -2 * n.dot(coord)
-    s1 = (p/2.) + np.sqrt( (p*p/4.) - q )
-    s2 = (p/2.) - np.sqrt( (p*p/4.) - q )
-    vector = coord + np.max([s1,s2])*n
-    #print('norm', np.linalg.norm(vector))
-    return vector
+        assert points.shape[0]==3
 
-def plot_cmap( triangle_corners ):
-    south_pole = np.array([0,0,-1])
-
-    ti = np.linalg.inv(triangle_corners.T)
-
-    xb, yb = ipf_bounds(triangle_corners, number_of_points=100)
-    xlow  = np.min(xb) - 0.02
-    xhigh = np.max(xb) + 0.02
-    ylow  = np.min(yb) - 0.02
-    yhigh = np.max(yb) + 0.02
-
-    normals, points = [],[]
-    c1,c2,c3 = triangle_corners
-    for cc in [(c1,c2),(c2,c3),(c3,c1)]:
-        normals.append( np.cross(cc[0],cc[1]) )
-        points.append( cc[0] )
-
-    resolution = 250
-    cmap = np.ones((resolution, resolution ,3))
-    for i,y in enumerate(np.linspace(yhigh, ylow, resolution)):
-        for j,x in enumerate(np.linspace(xlow, xhigh, resolution)):
-            coord = np.array([x,y,0])
-            vector = _backtrace(coord, south_pole)
-
-            in_symmetry_triangle = True
-            for nn,pp in zip(normals, points):
-                if (vector - pp).dot(nn) < -1e-8:
-                    in_symmetry_triangle = False
-                    break
-
-            if in_symmetry_triangle:
-                c = np.abs( ti.dot(vector) )
-                cmap[i,j,:] = c / np.max(c)
-
-    fig,ax = plt.subplots(1,1)
-    ax.imshow(cmap)
-    xt, yt = ipf_bounds(triangle_corners, number_of_points=25)
-    xt = (xt -xlow) / (xhigh - xlow)
-    xt = (resolution * xt)
-    yt = (-(yt - ylow) / (yhigh - ylow) ) + 1.0 - (0.5/resolution)
-    yt = (resolution * yt)
-    ax.plot(xt, yt, 'k-', linewidth=2.5)
-    xc, yc = ipf_corners(triangle_corners)
-    xc = (xc -xlow) / (xhigh - xlow)
-    xc = (resolution * xc)
-    yc = (-(yc -ylow) / (yhigh - ylow) ) + 1.0 - (0.5/resolution)
-    yc = (resolution * yc)
-    ax.text( xc[0]-0.05*resolution, yc[0]+0.1*resolution, str(triangle_corners[0,:]).replace(' ', '').replace(']', '').replace('[', ''), fontsize=24 )
-    ax.text( xc[1]-0.03*resolution, yc[1]+0.1*resolution, str(triangle_corners[1,:]).replace(' ', '').replace(']', '').replace('[', ''), fontsize=24 )
-    ax.text( xc[2]-0.033*resolution, yc[2]-0.035*resolution, str(triangle_corners[2,:]).replace(' ', '').replace(']', '').replace('[', ''), fontsize=24 )
-    ax.plot(xc, yc, 'ko', markersize=10)
-    ax.axis('off')
-
-    return cmap, fig, ax
-
-def ipf_corners(triangle_corners):
-    """Return the symmetry triangle corners of an inverse pole figure.
-    """
-    X, Y = [], []
-    for c in triangle_corners:
-        x,y = stereographic_projection(c)
-        X.append(x)
-        Y.append(y)
-    return X, Y
-
-def ipf_bounds(triangle_corners, number_of_points):
-    """Return gridded points on the inverse pole figure symmetry triangle.
-    """
-    norm_triangle_corners = triangle_corners / np.linalg.norm( triangle_corners, axis=1 ).reshape(3,1)
-    X, Y = [], []
-    for i in range(norm_triangle_corners.shape[0]):
-        c1 = norm_triangle_corners[i,:]
-        if i==2:
-            c2 = norm_triangle_corners[0,:]
+        if points.shape==(3,):
+            p = points.copy().reshape(3,1)
         else:
-            c2 = norm_triangle_corners[i+1,:]
+            p = points.copy()
 
-        axis = np.cross(c1, c2)
-        axis = axis / np.linalg.norm(axis)
-        angle = np.arccos( c1.dot(c2) )
-        rotator = RodriguezRotator(axis)
-        for ang in np.linspace(0, angle, number_of_points):
-            rax = rotator(c1, ang)
-            x,y = stereographic_projection(rax)
-            X.append(x)
-            Y.append(y)
-    return X, Y
+        plane_z_offset = 0
+        pole = np.array([0, 0, -1]).reshape(3, 1)
+        norm = np.linalg.norm(p, axis=0)
+        norm[norm==0]=1
+        pn = p /norm
 
-def stereographic_projection(vector):
-    """Steriographically project a vector unto the unit sphere equator plane (z=0).
-    ref: https://en.wikipedia.org/wiki/Stereographic_projection
+        s = (plane_z_offset - pole[2]) / (pole[2] - pn[2,:])
 
-        Args:
-            vector (:obj:`numpy array`): shape=(3,)
+        projection = (pole + s*(pole - pn))
 
-        Returns:
-            x,y (:obj:`tuple` of `float`), projected coordinates in equator plane.
-    """
-    normalised_vector = vector / np.linalg.norm( vector )
+        if projection.shape[1]==1:
+            return projection[0:2].flatten()
+        else:
+            return projection[0:2, :]
 
-    if np.abs(normalised_vector[2])<=1e-8:
-        intersection = normalised_vector
-    else:
-        south_pole = np.array([0,0,-1])
-        n = normalised_vector - south_pole
-        s = -south_pole[2] / n[2]
-        intersection = south_pole + s*n
+    def get_colors(self, points):
+        if points.shape==(3,):
+            p = points.copy().reshape(3,1)
+        else:
+            p = points.copy()
+        uvw = np.linalg.lstsq( self.fundamental_triangle, points, rcond=-1 )[0]
 
-    assert np.abs(intersection[2])<1e-8, str(intersection)
+        if len(uvw.shape)==1:
+            return ( uvw - np.min(uvw) ) / np.max( uvw-np.min(uvw) )
+        else:
+            return ( uvw - np.min(uvw, axis=0)  ) / ( np.max(uvw- np.min(uvw, axis=0), axis=0))
 
-    return intersection[0], intersection[1]
+    def _filter_fundamental_zone(self, points, number_of_orientations):
 
-def cartesian_to_polar(x, y):
-    th = np.arctan2(y, x)
-    r = np.sqrt(x*x + y*y)
-    return th, r
+        xy = self.stereographic_projection(points)
 
-def in_symmetry_triangle(vector, triangle_corners):
-    c1,c2,c3 = triangle_corners
-    for cc in [(c1,c2),(c2,c3),(c3,c1)]:
-        normal = np.cross(cc[0],cc[1])
-        point = cc[0]
-        if (vector - point).dot(normal) < -1e-8:
-            return False
-    return True
+        edges = self.get_fundamental_triangle( resolution=60 )
+        xmin,xmax = np.min(edges[0,:]), np.max(edges[0,:])
+        ymin,ymax = np.min(edges[1,:]), np.max(edges[1,:])
+        mask = ~( (xy[0,:] < xmin) + (xy[1,:] < ymin) + (xy[0,:] > xmax) +  (xy[1,:] > ymax) )
 
+        for i in range(len(mask)):
+            if mask[i]:
+                mask[i] = Point( xy[:,i] ).within( self._p )
 
-def inverse_pole_figure(ubi_matrices, crystal_system, triangle_corners):
-    """
-    """
-    sample_axis = np.array([0,0,1])
-    X, Y, colors = [], [], []
-    ti = np.linalg.inv(triangle_corners.T)
-    symmetry_rotations = symmetry.rotations(crystal_system)
+        if np.sum(mask)!=number_of_orientations:
+            self._p = Polygon( self.get_fundamental_triangle(resolution=900).T )
+            for i in range(len(mask)):
+                if mask[i]:
+                    mask[i] = Point( xy[:,i] ).within( self._p )
+        if np.sum(mask)!=number_of_orientations:
+            print('WARNING: Some points on the edge of the fundamental zone could not be resolved... ')
 
-    for ubi in ubi_matrices:
+        return points[:, mask]
 
-        u, b = tools.ub_to_u_b( np.linalg.inv(ubi) )
-        #u, b = tools.ubi_to_u_b(ubi)
-
-        for symrot in symmetry_rotations:
-
-            #u_new = symrot.dot(u)
-
-            u_new = u.dot(symrot)
-
-            #ubi_new = symrot.dot( ubi )
-
-            ubi_new = np.linalg.inv(u_new.dot(b))
-            sample_axis_in_crystal = ubi_new.dot( sample_axis )
-            sample_axis_in_crystal = sample_axis_in_crystal/ np.linalg.norm(sample_axis_in_crystal)
-
-            if sample_axis_in_crystal[2]<0:
-                continue
-
-            if not in_symmetry_triangle(sample_axis_in_crystal, triangle_corners):
-                pass
-
-            print(sample_axis_in_crystal)
-
-            x, y = stereographic_projection(sample_axis_in_crystal)
-            # th, r = cartesian_to_polar(x, y)
-
-            X.append( x )
-            Y.append( y )
-
-            c = np.abs( ti.dot(sample_axis_in_crystal) )
-            colors.append( c / np.max(c) )
-
-    return np.array(X), np.array(Y), colors
-
-if __name__=='__main__':
-
-    # ubi_matrices = []
-    # base_path = os.path.dirname(__file__)
-    # for layer in range(15,16):
-    #     gr = os.path.join(base_path, 'ubi_files', 'matrices_'+str(layer).zfill(3)+'.ubi')
-    #     ubi_matrices.extend( [ g.ubi for g in read_grain_file(gr)] )
-
-    #print(len(ubi_matrices))
-
-    B = tools.form_b_mat([1., 1., 1., 90., 90., 90.])
-
-    # u = np.array([[1,0,0],[0,1,0],[0,0,1]])
-    # ubi_matrices = [ np.linalg.inv(u.dot(B)) ]
-
-    ubi_matrices=[]
-    np.random.seed(3)
-    zhat = np.array([0,0,1])
-    U = Rotation.random(1).as_matrix()
-    UBIs = np.linalg.inv( U.dot(B) )
-    for ubi in UBIs: ubi_matrices.append(ubi)
-    # for ubi in UBIs:
-    #     hkl = ubi.dot(zhat)
-    #     n = hkl / np.linalg.norm(hkl)
-    #     hkl_dir = np.array([1,0,0])
-    #     hkl_dir =  hkl_dir / np.linalg.norm(hkl_dir)
-    #     if np.abs(n.dot(hkl_dir)-1)<0.01: # z-axis close to 111
-    #         ubi_matrices.append(ubi)
-
-    #u = np.array([[-1,0,0],[0,-1,0],[0,0,1]])
-    #ubi_matrices = [ np.linalg.inv(B) ]
-
-    triangle_corners = np.array([[0,0,1], [1,0,1], [1,1,1] ])
-    x, y, colors = inverse_pole_figure(ubi_matrices, crystal_system=7, triangle_corners=triangle_corners)
-    print(len(colors), len(colors)//24)
-    #cmap, fig_cmap, ax_cmap = plot_cmap( triangle_corners )
-
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    c = ax.scatter(x, y, c=colors, s=35, alpha=0.5)
-
-    xt, yt = ipf_bounds(triangle_corners, number_of_points=25)
-    c = ax.plot(xt, yt, 'k-')
-
-    xc, yc = ipf_corners(triangle_corners)
-
-    ax.text( xc[0]-0.02, yc[0]-0.04, str(triangle_corners[0,:]).replace(' ', '').replace(']', '').replace('[', ''), fontsize=24 )
-    ax.text( xc[1]-0.01, yc[1]-0.04, str(triangle_corners[1,:]).replace(' ', '').replace(']', '').replace('[', ''), fontsize=24 )
-    ax.text( xc[2]-0.015, yc[2]+0.015, str(triangle_corners[2,:]).replace(' ', '').replace(']', '').replace('[', ''), fontsize=24 )
-
-    for h in range(-3,4):
-        for k in range(-3,4):
-            for l in range(-3,4):
-                hkl = np.array([h,k,l])
-                sym = symmetry.rotations(crystal_system=7)
-
-                in_triangle = 0
-                for s in sym:
-                    v = s.dot(hkl)
-                    if in_symmetry_triangle(v, triangle_corners):
-                        in_triangle +=1
-                if in_triangle!=1:
-                    print(hkl, in_triangle)
-                    print(sym)
+    def get_reciprocal_points(self, orientations, filter_by_fundamental_zone=True):
+        points = []
+        for U in orientations:
+            hkl = U.T @ self.view_axis
+            points.extend( list(self.P @ hkl) )
+        points = np.array(points).T
+        points = np.concatenate( (points, -points), axis=1 )
+        if filter_by_fundamental_zone:
+            return self._filter_fundamental_zone(points, len(orientations))
+        else:
+            return points
 
 
-
-    ax.axis('off')
-
-    plt.show()
+if __name__ == "__main__":
+    ipf = inverse_pole_figure(crystal_system='trigonal', view_axis= np.array([0,0,1]))
+    ipf.show( [Rotation.random().as_matrix().T for _ in range(1000)] )
+    ipf.colorbar()
